979639a7c049f91fe12f67c9ee1c0016157b199e
pestophagous <pestophagous@users.noreply.github.com>
2020-05-23 15:15:29 -0700

    use repeatable random seed in tests

--- src/util/random_concrete.cc
+++ src/util/random_concrete.cc
@@ -9,9 +9,15 @@
 
 namespace heory
 {
+namespace
+{
+    constexpr std::mt19937::result_type CONST_SEED = 3;
+} // namespace
+
 struct Distribution
 {
-    Distribution( int lower, int upper ) : gen( rd() ), dis( lower, upper )
+    Distribution( int lower, int upper, bool repeatableSeed )
+        : gen( repeatableSeed ? CONST_SEED : rd() ), dis( lower, upper )
     {
         FASSERT(
             upper > lower, "we need an ordered pair of distinct values, least to greatest" );
@@ -27,12 +33,23 @@ struct Distribution
     std::uniform_int_distribution<> dis;
 };
 
+/*static*/ std::unique_ptr<RandomConcrete> RandomConcrete::RandomConcreteForProduction()
+{
+    return std::unique_ptr<RandomConcrete>( new RandomConcrete( false /*repeatableSeed*/ ) );
+}
+
+/*static*/ std::unique_ptr<RandomConcrete> RandomConcrete::RandomConcreteForTests()
+{
+    return std::unique_ptr<RandomConcrete>( new RandomConcrete( true /*repeatableSeed*/ ) );
+}
+
 struct RandomConcrete::Impl
 {
     std::map<std::pair<int, int>, std::unique_ptr<Distribution>> m_distributions;
 };
 
-RandomConcrete::RandomConcrete() : m_impl( std::make_unique<Impl>() )
+RandomConcrete::RandomConcrete( const bool repeatableSeed )
+    : m_usingRepeatableSeed( repeatableSeed ), m_impl( std::make_unique<Impl>() )
 {
 }
 
@@ -57,7 +74,8 @@ int RandomConcrete::GetNextFromNToMInclusive( const int n, const int m )
     }
     else
     {
-        m_impl->m_distributions[ pair ] = std::make_unique<Distribution>( n, m );
+        m_impl->m_distributions[ pair ]
+            = std::make_unique<Distribution>( n, m, m_usingRepeatableSeed );
         return m_impl->m_distributions[ pair ]->GetNext();
     }
 }
