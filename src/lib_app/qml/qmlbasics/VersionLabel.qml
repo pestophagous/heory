pestophagous/heory

commit e3d88d9d0d358223df2c3169d3a5e608f88d1743

refactor all build scripts to enable Qt Android build

src/lib_app/qml/qmlbasics/VersionLabel.qml
@@ -5,7 +5,20 @@ import libstyles 1.0
 import ".."
 
 Label {
+  property bool verBool_: versionInfoGitHash
+  property string ver_: (!!verBool_) ? versionInfoGitHash : "$git-info-here$"
+
+  property bool builddateBool_: versionInfoBuildDateString
+  property string builddate_: {
+    if (!!builddateBool_) {
+      versionInfoBuildDateString
+    } else {
+      Qt.formatDateTime(new Date(), "yyyy-MM-dd")
+    }
+  }
+
   font: Theme.basicFont
-  color: Theme.neutralMediumLight
-  text: versionInfoGitHash + " - Built on: " + versionInfoBuildDateString
+  text: ver_ + " - Built on: " + builddate_
+  horizontalAlignment: Text.AlignHCenter
+  verticalAlignment: Text.AlignVCenter
 }

.github/workflows/main.yml
@@ -5,7 +5,7 @@
 # https://www.219design.com
 # Software | Electrical | Mechanical | Product Design
 #
-name: CI
+name: Linux_CI
 
 on: [push]
 
@@ -24,8 +24,11 @@ jobs:
         for apt_file in `grep -lr microsoft /etc/apt/sources.list.d/`; do sudo rm $apt_file; done
 
     - if: runner.os == 'Linux'
-      # Fix (another kind) of azure ubuntu apt repo issue
+      # Fix (another kind) of azure ubuntu apt repo issue. (Unable to connect to azure.archive.ubuntu.com)
       run: sed -e 's/azure.archive.ubuntu.com/us.archive.ubuntu.com/g' -e t -e d /etc/apt/sources.list | sudo tee /etc/apt/sources.list.d/nonazure.list
 
     - name: run_all_tests
+      env:
+        MYAPP_TEMPLATE_BUILD_ANDROID: 1
+        MYAPP_TEMPLATE_BUILD_APPIMAGE: 1
       run: ./run_all_tests.sh

.gitignore
@@ -1,4 +1,11 @@
+dl_third_party
 build
 AppImage_staging
 linuxdeployqt-6-x86_64.AppImage
 gui_test.log
+
+# https://github.com/miurahr/aqtinstall
+aqtinstall.log
+
+# mac:
+**/.DS_Store

.gitmodules
@@ -1,6 +1,3 @@
-[submodule "build_qt_binaries"]
-	path = build_qt_binaries
-	url = https://github.com/219-design/build_qt_binaries.git
 [submodule "fluidsynth"]
 	path = fluidsynth
 	url = https://github.com/pestophagous/fluidsynth.git

android.pri
@@ -1,0 +1,5 @@
+our_android_lib_suffix=""
+
+android {
+    our_android_lib_suffix="_$${ANDROID_TARGET_ARCH}"
+}

build_app.sh
@@ -16,14 +16,14 @@ trap "popd" EXIT HUP INT QUIT TERM
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR  # enter this script's directory. (in case called from root of repository)
 
-# adding this next line here (rather than in run_all_tests.sh), because we think
-# of the graph-generation as more of a "build" step than a "test":
-$DIR/sw_arch_doc/generate_graph.sh
-
-if [ -f build_qt_binaries/.git ]; then
-  echo "Skipping any work in qt5 bin submodule. Looks done already."
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+  exit 1
 else
-  git submodule update --init --recursive build_qt_binaries
+  qmakecmd=qmake
+  MYAPP_EXTRA_CONF=""
+  # adding this next line here (rather than in run_all_tests.sh), because we think
+  # of the graph-generation as more of a "build" step than a "test":
+  $DIR/sw_arch_doc/generate_graph.sh
 fi
 
 if [ -f oboe/.git ]; then
@@ -49,6 +49,9 @@ else
   git submodule update --init --recursive fluidsynth
 fi
 
+pushd fluidsynth >& /dev/null
+  git checkout CMakeLists.txt # in case our last Android build left it dirty
+popd >& /dev/null
 mkdir -p build/fsynth
 pushd build/fsynth >& /dev/null
   cmake --version # to print the version to CI logs.
@@ -59,27 +62,36 @@ popd >& /dev/null
 cp fluidsynth/src/midi/fluid_midi.h build/fsynth/gcc_64/usr/local/include/fluidsynth/ # TODO: is this supposed to be a private header? i need the NOTE_ON constant
 patch build/fsynth/gcc_64/usr/local/include/fluidsynth/fluid_midi.h patch_fluid_midi.h.patch
 
-tools/ci/build_oboe_for_android.sh # TODO: make conditional upon running android build
-
 mkdir -p build/generated_files/autogenerated
-tools/update_version_strings.sh tools/version.h build/generated_files/autogenerated/version.h
+tools/update_version_strings.sh src/version.genfiles.input/version.h build/generated_files/autogenerated/version.h
 
 source $DIR/path_to_qmake.bash
 
-cd build
-qmake "$DIR"
+pushd build >& /dev/null
 
-if [[ -n ${GITHUB_ACTIONS-} || -n ${BITBUCKET_REPO_OWNER-} || -n ${BITBUCKET_REPO_FULL_NAME-} ]];
-then
-  # we have to skip the apptest(s) on GitHub for now (alsa dummy module won't load)
-  make CPPFLAGS_DEF_QSTYLE="HEORY_APPTEST_SKIPTHIS"
-else
-  # https://stackoverflow.com/questions/17578150/add-cflags-to-qmake-project-without-hard-coding-them-in-the-pro-file
-  # https://stackoverflow.com/questions/7754218/qmake-how-to-add-and-use-a-variable-into-the-pro-file
-  make CPPFLAGS_DEF_QSTYLE="HEORY_APPTEST"
+  # When you need release: CONFIG+=release
+  ${qmakecmd} $MYAPP_EXTRA_CONF CONFIG+=force_debug_info "$DIR" # note: debug INFO (symbols) are ok even in release
+
+  if [[ -n ${GITHUB_ACTIONS-} || -n ${BITBUCKET_REPO_OWNER-} || -n ${BITBUCKET_REPO_FULL_NAME-} ]];
+  then
+    # we have to skip the apptest(s) on GitHub for now (alsa dummy module won't load)
+    make CPPFLAGS_DEF_QSTYLE="HEORY_APPTEST_SKIPTHIS"
+  else
+    # https://stackoverflow.com/questions/17578150/add-cflags-to-qmake-project-without-hard-coding-them-in-the-pro-file
+    # https://stackoverflow.com/questions/7754218/qmake-how-to-add-and-use-a-variable-into-the-pro-file
+    make CPPFLAGS_DEF_QSTYLE="HEORY_APPTEST"
+  fi
+
+  make install # puts necessary items side-by-side with app exe
+
+popd >& /dev/null
+
+if [[ -n ${MYAPP_TEMPLATE_BUILD_ANDROID-} ]]; then
+  tools/ci/get_android_toolchain.sh
+  tools/ci/build_oboe_for_android.sh
+  tools/ci/build_fluidsynth_for_android.sh
+  tools/ci/build_android_app.sh build/for_android "CONFIG+=force_debug_info"
 fi
-
-make install # puts necessary items side-by-side with app exe
 
 pushd $DIR/build/src/apptest >& /dev/null
   rm collection.o

compiler_flags.pri
@@ -6,93 +6,129 @@
 # Software | Electrical | Mechanical | Product Design
 #
 
-QMAKE_CXX = g++-7
-QMAKE_LINK = g++-7
-QMAKE_CC = gcc-7
-CONFIG += c++17
-
-# the line below suppresses warnings generated by Qt's header files: we tell
-# GCC to treat Qt's headers as "system headers" with the -isystem flag
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtConcurrent
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtCore
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtFontDatabaseSupport
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtGui
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtHelp
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtMultimedia
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtMultimediaQuick
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtMultimediaWidgets
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQml
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQmlDebug
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQmlModels
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQuick
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQuickControls2
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQuickWidgets
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtTest
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtWidgets
-QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtXml
-
-unix:{
-    # So the exe will launch if we put all our '*.so' dylibs side-by-side with it.
-    # Based on https://stackoverflow.com/a/27393241/10278
-    QMAKE_LFLAGS += "-Wl,-rpath,\'\$$ORIGIN\'"
+linux:!android {
+  greaterThan(QT_MAJOR_VERSION, 5) {
+    QMAKE_CXX = g++-9
+    QMAKE_LINK = g++-9
+    QMAKE_CC = gcc-9
+  } else {
+    QMAKE_CXX = g++-7
+    QMAKE_LINK = g++-7
+    QMAKE_CC = gcc-7
+  }
 }
 
-unix:{
-    # some inspired by: https://kristerw.blogspot.com/2017/09/useful-gcc-warning-options-not-enabled.html
-    # others inspired by: https://stackoverflow.com/questions/5088460/flags-to-enable-thorough-and-verbose-g-warnings
-    QMAKE_CXXFLAGS += "\
-         -Wall \
-         -Wcast-align \
-         -Wcast-qual \
-         -Wconversion \
-         -Wdisabled-optimization \
-         -Wduplicated-branches \
-         -Wduplicated-cond \
-         -Werror \
-         -Werror=switch \
-         -Wextra \
-         -Wfloat-equal \
-         -Wformat-nonliteral \
-         -Wformat-security \
-         -Wformat=2 \
-         -Wimport \
-         -Winit-self \
-         -Winline \
-         -Wlogical-op \
-         -Wmisleading-indentation \
-         -Wmissing-declarations \
-         -Wmissing-field-initializers \
-         -Wmissing-format-attribute   \
-         -Wmissing-include-dirs \
-         -Wmissing-noreturn \
-         -Wnon-virtual-dtor \
-         -Wnull-dereference \
-         -Wold-style-cast \
-         -Wpedantic \
-         -Wpointer-arith \
-         -Wredundant-decls \
-         -Wrestrict \
-         -Wshadow \
-         -Wshadow \
-         -Wstack-protector \
-         -Wswitch-default \
-         -Wswitch-enum \
-         -Wundef  \
-         -Wuninitialized \
-         -Wunreachable-code \
-         -Wunused \
-         -Wunused-but-set-parameter \
-         -Wunused-parameter \
-         -Wvariadic-macros \
-         -Wwrite-strings \
-         "
+CONFIG += c++17
 
-    # Do the 'no-' flags in their own statement, to ensure that they always happen last:
+ios|macx {
+    # the lines below suppress warnings generated by Qt's header files: we tell
+    # Clang to treat Qt's (mac) Framework headers as "system headers":
+    QMAKE_CXXFLAGS += -iframework $$[QT_INSTALL_LIBS]
+    QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_LIBS]/QtCore.framework/Headers
+}
+
+!win32 {
+  # the lines below suppress warnings generated by Qt's header files: we tell
+  # GCC/Clang to treat Qt's headers as "system headers" with the -isystem flag
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtConcurrent
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtCore
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtGui
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtHelp
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQml
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQmlDebug
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQmlModels
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQuick
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQuickControls2
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtQuickWidgets
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtTest
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtWidgets
+  QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtXml
+
+  lessThan(QT_MAJOR_VERSION, 6) {
+      QMAKE_CXXFLAGS += -isystem $$[QT_INSTALL_HEADERS]/QtFontDatabaseSupport
+  }
+
+
+  unix:!android {
+      # So the exe will launch if we put all our '*.so' dylibs side-by-side with it.
+      # Based on https://stackoverflow.com/a/27393241/10278
+      QMAKE_LFLAGS += "-Wl,-rpath,\'\$$ORIGIN\'"
+  }
+
+  linux:!android {
+      QMAKE_CXXFLAGS += "\
+           -Wduplicated-branches \
+           -Wduplicated-cond \
+           -Wlogical-op \
+           -Wmisleading-indentation \
+           -Wrestrict \
+           -Wunused-but-set-parameter \
+           "
+  }
+
+  !android {
+      QMAKE_CXXFLAGS += "\
+          -Werror \
+          "
+  }
+
+  # some inspired by: https://kristerw.blogspot.com/2017/09/useful-gcc-warning-options-not-enabled.html
+  # others inspired by: https://stackoverflow.com/questions/5088460/flags-to-enable-thorough-and-verbose-g-warnings
+  QMAKE_CXXFLAGS += "\
+       -Wall \
+       -Wcast-align \
+       -Wcast-qual \
+       -Wconversion \
+       -Wdisabled-optimization \
+       -Werror=switch \
+       -Wextra \
+       -Wfloat-equal \
+       -Wformat-nonliteral \
+       -Wformat-security \
+       -Wformat=2 \
+       -Wimport \
+       -Winit-self \
+       -Winline \
+       -Wmissing-declarations \
+       -Wmissing-field-initializers \
+       -Wmissing-format-attribute   \
+       -Wmissing-include-dirs \
+       -Wmissing-noreturn \
+       -Wnon-virtual-dtor \
+       -Wnull-dereference \
+       -Wold-style-cast \
+       -Wpedantic \
+       -Wpointer-arith \
+       -Wredundant-decls \
+       -Wshadow \
+       -Wstack-protector \
+       -Wswitch-default \
+       -Wswitch-enum \
+       -Wundef  \
+       -Wuninitialized \
+       -Wunreachable-code \
+       -Wunused \
+       -Wunused-parameter \
+       -Wvariadic-macros \
+       -Wwrite-strings \
+       "
+
+  # Do the 'no-' flags in their own statement, to ensure that they always happen last:
+  QMAKE_CXXFLAGS += "\
+       -Wno-error=format-nonliteral \
+       -Wno-error=inline \
+       "
+  # Rationale for 'Wno-error=format-nonliteral': we trigger this in rc/minutil/logger.cc (TODO: eventually suppress this ONLY for that cc file)
+  # Rationale for 'Wno-error=inline' is that a failure to inline is information, and never truly 'wrong'
+}
+
+ios|macx {
+    # Disable a couple that are more onerous to comply with on Mac
     QMAKE_CXXFLAGS += "\
-         -Wno-error=inline \
-         "
-    # Rationale for 'Wno-error=inline' is that a failure to inline is information, and never truly 'wrong'
+        -Wno-error=missing-noreturn \
+        -Wno-error=sign-conversion \
+        "
 }
 
 !include($$top_srcdir/src/assert/assert.pri) { error() } # allows all code to include util-assert.h

cross_platform.pri
@@ -1,0 +1,2 @@
+!include($$top_srcdir/android.pri) { error() }
+!include($$top_srcdir/windows.pri) { error() }

commit c2d5838a7265765c084b04bb7f4367f781aa64dd

restore font color to VersionLabel.qml (broken in e3d88d9d0d3)

src/lib_app/qml/qmlbasics/VersionLabel.qml
@@ -18,6 +18,7 @@ Label {
   }
 
   font: Theme.basicFont
+  color: Theme.neutralMediumLight
   text: ver_ + " - Built on: " + builddate_
   horizontalAlignment: Text.AlignHCenter
   verticalAlignment: Text.AlignVCenter