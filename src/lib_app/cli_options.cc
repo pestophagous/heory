04af929cefdef346a6de812afd3f7baaf94f1b18
pestophagous <pestophagous@users.noreply.github.com>
2021-05-22 16:52:40 -0700

    allow use of our CliOptions when launching the 'project aware' qmlscene

--- src/lib_app/cli_options.cc
+++ src/lib_app/cli_options.cc
@@ -7,6 +7,7 @@
 #include "cli_options.h"
 
 #include <QCommandLineParser>
+#include <QDebug>
 
 namespace heory
 {
@@ -14,13 +15,8 @@ constexpr char GUI_TEST_CLI_OPT[] = "guitest";
 constexpr char MAX_QT_LOGGING_CLI_OPT[] = "maxlogs";
 constexpr char GUI_TEST_AMIDI_PORT_OPTSTRING[] = "argforamidi";
 
-CliOptions::CliOptions( bool dummyObject, const QCoreApplication& app )
+CliOptions::CliOptions( const bool dummyObject, const QCoreApplication& app )
 {
-    if( dummyObject )
-    {
-        return;
-    }
-
     QCommandLineParser parser;
     parser.addHelpOption();
 
@@ -38,7 +34,25 @@ CliOptions::CliOptions( bool dummyObject, const QCoreApplication& app )
         argForAmidi} );
     // clang-format on
 
-    parser.process( app );
+    if( !dummyObject )
+    {
+        // The NOT-dummy case is the full desktop application.
+        parser.process( app );
+    }
+    else
+    {
+        // The 'dummy' case (not the most accurate adjective) is the case when
+        // this instance of CliOptions is part of our 'lightly hacked' and
+        // 'project aware' locally-built qmlscene.
+        //    We MUST NOT CALL QCommandLineParser::process IN THIS CASE, because it
+        // will prevent us from using the arguments that qmlscene accepts in its
+        // own 'main' function.
+        //    By calling 'parse' instead, we DO achieve the desired capture of
+        // our app-specific options. As a trade-off, we forgo the built-in
+        // type-checking and error reporting of QCommandLineParser::process
+        const QStringList arguments = QCoreApplication::arguments();
+        parser.parse( arguments );
+    }
 
     m_guiTests = parser.isSet( GUI_TEST_CLI_OPT );
     m_maximumQtLogs = parser.isSet( MAX_QT_LOGGING_CLI_OPT );
