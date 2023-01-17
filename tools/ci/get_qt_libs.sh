2ea239ff5af8f689c38e64478e0d261ae442362c
pestophagous <pestophagous@users.noreply.github.com>
2023-01-17 10:44:12 -0800

    fix use of aqt

--- tools/ci/get_qt_libs.sh
+++ tools/ci/get_qt_libs.sh
@@ -8,40 +8,39 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 CUR_GIT_ROOT=$(git rev-parse --show-toplevel)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DL_FOLDER=$CUR_GIT_ROOT/dl_third_party
+if [[ -n ${MYAPP_TEMPLATE_DL_FOLDER_OVERRIDE-} ]]; then
+  DL_FOLDER=${MYAPP_TEMPLATE_DL_FOLDER_OVERRIDE}
+fi
 
-QTMIRROR='http://mirrors.ocf.berkeley.edu/qt/'
+# --- Other mirrors (for times of trouble) -------
+# http://qt-mirror.dannhauer.de/
+# http://mirrors.ukfast.co.uk/sites/qt.io
+# http://mirror.csclub.uwaterloo.ca/qtproject/
+
+#QTMIRROR='http://mirrors.ocf.berkeley.edu/qt/' # was being slow, 2021-Mar-24
+QTMIRROR='http://ftp.fau.de/qtproject'
 QTSIXMIRROR='http://ftp.fau.de/qtproject'
 
-sudo pip3 install aqtinstall  # https://github.com/miurahr/aqtinstall
+pip3 install --upgrade pip
+pip3 install setuptools
+pip3 install -r ${DIR}/for_pip/requirements.txt # install aqtinstall
 
-if [ -d $DL_FOLDER/Qt_desktop/5.15.0/gcc_64/bin ]; then
-    echo "no need to download qt5 for desktop"
+if [ -d $DL_FOLDER/Qt_desktop/6.2.2/gcc_64/bin ]; then
+    echo "no need to download qt for desktop"
 else
 
   if [[ -n ${MYAPP_TEMPLATE_QT6-} ]]; then
     # https://github.com/miurahr/aqtinstall/issues/126 "Installing smaller subset of the libraries"
-    python3 -m aqt install --base "${QTSIXMIRROR}" --outputdir $DL_FOLDER/Qt_desktop 6.0.0 linux desktop --archives \
-     icu \
-     qtbase \
+    python3 -m aqt install-qt --base "${QTSIXMIRROR}" --outputdir $DL_FOLDER/Qt_desktop linux desktop 6.2.2 --modules \
      qtconnectivity \
-     qtdeclarative \
-     qtgraphicaleffects \
      qtimageformats \
-     qtquickcontrols \
-     qtquickcontrols2 \
-     qtsvg \
-     qttools
-
-    # we still need qt5 in order to execute qmlfmt. (until we can recompile qmlfmt with qt6)
-    # https://github.com/miurahr/aqtinstall/issues/126 "Installing smaller subset of the libraries"
-    python3 -m aqt install --base "${QTMIRROR}" --outputdir $DL_FOLDER/Qt_desktop 5.15.0 linux desktop --archives \
-     icu \
-     qtbase
+     qt5compat
   else
     # https://github.com/miurahr/aqtinstall/issues/126 "Installing smaller subset of the libraries"
-    python3 -m aqt install --base "${QTMIRROR}" --outputdir $DL_FOLDER/Qt_desktop 5.15.0 linux desktop --archives \
+    python3 -m aqt install-qt --base "${QTMIRROR}" --outputdir $DL_FOLDER/Qt_desktop linux desktop 5.15.0 --archives \
      icu \
      qtbase \
      qtconnectivity \
